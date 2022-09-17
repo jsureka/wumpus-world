65babac4ca77a0878f104f415ddadb494d8fba29
jsureka <bsse1115@iit.du.ac.bd>
2022-09-17 16:11:14 +0600

    gui and logic implemented

---
+++ constants.py
@@ -0,0 +1,28 @@
+
+# ------------------------
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHTGREY = (200, 200, 200)
+# ------------------------
+WUMPUS_ICON = './res/wumpus.png'
+WUMPUS = './res/wumpus_ico.png'
+GOLD = './res/gold.png'
+PIT = './res/pit.png'
+
+PLAYER_DOWN = './res/player_facing_to_down.png'
+PLAYER_UP = './res/player_facing_to_up.png'
+PLAYER_LEFT = './res/player_facing_to_left.png'
+PLAYER_RIGHT = './res/player_facing_to_right.png'
+
+VISIBLE_FLOOR = './res/floor.png'
+HIDDEN_FLOOR = './res/floor_1.png'
+# -----------------------------
+CAPTION = "Wumpus World"
+# -----------------------------
+WUMPUS_COUNT = 1
+PIT_COUNT = 10
+GOLD_COUNT = 10
+# -----------------------------
+ROW_COUNT = 10
+COL_COUNT = 10
+TILE_SIZE = 64
